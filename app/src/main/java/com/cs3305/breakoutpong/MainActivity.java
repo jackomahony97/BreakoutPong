267b28bc98992f5b858e140e323e271af2336d77
jacko <jackomahony97@gmail.com>
2020-02-05 19:38:01 +0000

    Changed colour resources

--- app/src/main/java/com/cs3305/breakoutpong/MainActivity.java
+++ app/src/main/java/com/cs3305/breakoutpong/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); //commit test
         setContentView(R.layout.activity_main);
     }
 }
