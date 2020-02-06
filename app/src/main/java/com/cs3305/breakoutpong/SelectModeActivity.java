257df1430a64388061979b73b4e6f14444f3899a
jacko <jackomahony97@gmail.com>
2020-02-06 18:50:59 +0000

    Added xml file for SelectModeActivity and added single and 2-player game activitys and their xml files

--- app/src/main/java/com/cs3305/breakoutpong/SelectModeActivity.java
+++ app/src/main/java/com/cs3305/breakoutpong/SelectModeActivity.java
@@ -2,13 +2,51 @@ package com.cs3305.breakoutpong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 public class SelectModeActivity extends AppCompatActivity {
+
+    Spinner dropdown;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_mode);
+
+
+        //get the spinner from the xml.
+        dropdown = findViewById(R.id.et_mode);
+        //create a list of items for the spinner.
+        String[] items = new String[]{"Single", "2-Player"};
+        //create an adapter to describe how the items are displayed, adapters are used in several places in android.
+        //There are multiple variations of this, but this is the basic variant.
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        //set the spinners adapter to the previously created one.
+        dropdown.setAdapter(adapter);
+
+        btn = findViewById(R.id.btn_continue_mode);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String text = dropdown.getSelectedItem().toString();
+                if (text.equals("Single")) {
+                    Intent myIntent = new Intent(SelectModeActivity.this, SingleGameActivity.class);
+                    // myIntent.putExtra("key", value); //Optional parameters
+                    SelectModeActivity.this.startActivity(myIntent);
+                }
+                else if (text.equals("2-Player")) {
+                    Intent myIntent = new Intent(SelectModeActivity.this, TwoPlayerGameActivity.class);
+                    // myIntent.putExtra("key", value); //Optional parameters
+                    SelectModeActivity.this.startActivity(myIntent);
+                }
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/cs3305/breakoutpong/TwoPlayerGameActivity.java
@@ -0,0 +1,14 @@
+package com.cs3305.breakoutpong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TwoPlayerGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_player_game);
+    }
+}
