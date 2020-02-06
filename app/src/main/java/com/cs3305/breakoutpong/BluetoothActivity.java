7277ca35934b95fdeb4d7fe44e172c2221e63853
jacko <jackomahony97@gmail.com>
2020-02-06 19:37:57 +0000

    Added Paired Bluetooth devices to dropdown menu of SelectModeActivity

--- app/src/main/java/com/cs3305/breakoutpong/BluetoothActivity.java
+++ app/src/main/java/com/cs3305/breakoutpong/BluetoothActivity.java
@@ -2,6 +2,8 @@ package com.cs3305.breakoutpong;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,21 +11,49 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import java.util.Set;
+
 public class BluetoothActivity extends AppCompatActivity {
 
     Spinner dropdown;
     Button btn;
+    BluetoothAdapter bluetoothAdapter;
+    int REQUEST_ENABLE_BLUETOOTH = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
 
+    //
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BLUETOOTH);
+        }
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        int i = 0;
+        String[] items = new String[pairedDevices.size()]; //make an array (size of paired devices)
+
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                items[i] = deviceName;
+                i++;
+            }
+        }
+
 
         //get the spinner from the xml.
         dropdown = findViewById(R.id.et_bl_devices);
         //create a list of items for the spinner.
-        String[] items = new String[]{"1", "2", "three"};
         //create an adapter to describe how the items are displayed, adapters are used in several places in android.
         //There are multiple variations of this, but this is the basic variant.
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
@@ -36,7 +66,7 @@ public class BluetoothActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String text = dropdown.getSelectedItem().toString();
-                if (text.equals("three")) {
+                if (text.equals("HC05")) {
                     Intent myIntent = new Intent(BluetoothActivity.this, SelectModeActivity.class);
                     // myIntent.putExtra("key", value); //Optional parameters
                     BluetoothActivity.this.startActivity(myIntent);
